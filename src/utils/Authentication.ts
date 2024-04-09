b797cd62d987c72969575d6ea023f8ae2ab0b3ab
Katryel Menezes <katryelmenezesvieira14@gmail.com>
2024-04-09 00:56:16 -0300

    update: .env.example

--- src/utils/Authentication.ts
+++ src/utils/Authentication.ts
@@ -2,7 +2,7 @@ import { Request as Req, Response as Res, NextFunction as Next } from "express";
 
 import { UnauthorizedError as AuthError } from "./AppError";
 
-const SECRET_KEY = process.env.SECRET_KEY || "8475bb04c8090afd7153aca3f0efa9a3";
+const SECRET_KEY = process.env.SECRET_KEY || "your_secret_key";
 
 export function Authentication(req: Req, res: Res, next: Next) {
   const token = req.headers.authorization;
